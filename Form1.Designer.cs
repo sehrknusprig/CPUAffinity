deaf3af54ab21966d193875350d7a322d2143513
Sehr Knusprig <info@sehrknusprig.de>
2020-07-20 20:29:21 +0200

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CPUAffinity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 190);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0x0000000000000000";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(320, 169);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.SelectedValueChanged += new System.EventHandler(this.checkedListBox1_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 223);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPUAffinity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int c = 0; c < 64; c++)
+            {
+                checkedListBox1.Items.Add("CPU " + c);
+            }
+        }
+
+        private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            this.BeginInvoke(new Action(() =>
+            {
+                calculateAffinity();
+            }));
+        }
+
+        private void calculateAffinity()
+        {
+            int mask_low = 0;
+            int mask_high = 0;
+
+            for (int c = 0; c < checkedListBox1.Items.Count; c++)
+            {
+                if (checkedListBox1.GetItemChecked(c))
+                {
+                    Console.WriteLine("been here " + c);
+                    if (c < 32)
+                    {
+                        mask_low = mask_low | (1 << c);
+                    }
+                    else
+                    {
+                        mask_high = mask_high | (1 << c);
+                    }
+                }
+            }
+
+            textBox1.Text = "0x" + long2hex(mask_high).ToUpper() + long2hex(mask_low).ToUpper();
+        }
+
+        private string pad0(string num, int width)
+        {
+            string zeros = "";
+            for (int i = 0; i < width; i++)
+            {
+                zeros += "0";
+            }
+
+            string zeronum = (zeros + num);
+            return zeronum.Substring(zeronum.Length - width);
+        }
+
+        private string long2hex(int num)
+        {
+            return pad0(Convert.ToString((int)((uint)num >> 24), 16), 2) +
+                    pad0(Convert.ToString(num >> 16 & 255, 16), 2) +
+                    pad0(Convert.ToString(num >> 8 & 255, 16), 2) +
+                    pad0(Convert.ToString(num & 255, 16), 2);
+        }
+
+        private void checkedListBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
